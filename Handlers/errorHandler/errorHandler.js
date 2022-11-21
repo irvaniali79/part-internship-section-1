25e9ce5e9d5e30d8efbfbe5db86433b26c0c5c75
ali.iravani <irvaniali79@gmail.com>
2022-11-21 12:07:05 +0330

    refactor(eslint): cleaning by eslint

--- Handlers/errorHandler/errorHandler.js
+++ Handlers/errorHandler/errorHandler.js
@@ -1,46 +1,49 @@
 
 function errorHandler(req,res,e){
-    const Errors = {
-        '404':()=>{
-            res.writeHead(404, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-                message:"Page not found"
-            }));
-        },
-        '401':()=>{
-            res.writeHead(401, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-                message:"Unauthorize request"
-            }));
-        },
-        '429':()=>{
-            res.writeHead(429, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-                message:"Too many request"
-            }));
-        },
-        '500':()=>{
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-                message:"Internal server error"
-            }));
-        },
-        'database':()=>{
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({
-                message:"Internal server error - models"
-            }));
-        },
-        'default':()=>{
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(e.message));
-        }
-
-    }    
-    if(!(e.code in Errors)){
-        Errors['default']();
-        return;
+  const Errors = {
+    '404':()=>{
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message:'Page not found'
+      }));
+    },
+    '401':()=>{
+      res.writeHead(401, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message:'Unauthorize request'
+      }));
+    },
+    '429':()=>{
+      res.writeHead(429, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message:'Too many request'
+      }));
+    },
+    '500':()=>{
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message:'Internal server error'
+      }));
+    },
+    'database':()=>{
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message:e.message
+      }));
+    },
+    'default':()=>{
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        status:'error',
+        message:e.message
+      }));
     }
-    Errors[e.code]();
-};
+
+  };    
+  if(!(e.code in Errors)){
+    Errors['default']();
+    return;
+  }
+  Errors[e.code]();
+}
 module.exports = errorHandler;
--- middlewares/index.js
+++ middlewares/index.js
@@ -3,4 +3,4 @@ const fetchFromParams = require('./fetchFromParams');
 const fetchFromBody = require('./fetchFromBody');
 const fetchFileFromFormData = require('./fetchFileFromFormData');
 
-module.exports = {fetchQueryStringFromURL,fetchFileFromFormData,fetchFromParams,fetchFromBody}
+module.exports = {fetchQueryStringFromURL,fetchFileFromFormData,fetchFromParams,fetchFromBody};
