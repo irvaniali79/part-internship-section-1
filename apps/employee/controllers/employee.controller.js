312f94ab1f73c8a1c5af2437b867f951f68bd94d
ali.iravani <irvaniali79@gmail.com>
2022-11-26 16:39:06 +0330

    refactor(cleanCode): divide insert to 2 method and add method as type into res

--- apps/employee/controllers/employee.controller.js
+++ apps/employee/controllers/employee.controller.js
@@ -2,7 +2,11 @@ const employeeRepository = require('../repositories/employeeRepository');
 
 async function add(req,res){
   const {id,data,parent} = req.body;
-  await employeeRepository.insert({id,data,parentId:parent});
+  
+  if (parent)await employeeRepository.insert({id,data,parentId:parent});
+  else await employeeRepository.insertParent({id,data});
+
+  res.type('POST');
   res.response(JSON.stringify({
     status:'success',
     message:'your data cached successfully'
@@ -12,6 +16,8 @@ async function add(req,res){
 
 async function get(req,res,queryParams){
   const result = await employeeRepository.fetch({id:queryParams[0]});
+
+  res.type('GET');
   res.response(JSON.stringify({
     status:'success',
     data:result
@@ -21,6 +27,7 @@ async function get(req,res,queryParams){
 async function edit(req,res){
   const {id,data,parent} = req.body;
   const result = await employeeRepository.update({id,data,parentId:parent});
+  res.type('PUT');
   res.response(JSON.stringify({
     status:'success',
     message:'your data changed successfully',
@@ -32,5 +39,4 @@ module.exports = {
   add,
   get,
   edit
-
 };
