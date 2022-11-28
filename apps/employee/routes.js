c87fbae1c71aa8ab4657d5362f99fe7e85ffa88f
ali.iravani <irvaniali79@gmail.com>
2022-11-28 13:31:22 +0330

    fix(routes): add / to end of all routes

--- apps/employee/routes.js
+++ apps/employee/routes.js
@@ -2,9 +2,9 @@ const {app} = require('../..');
 const employeeController = require('./controllers/employee.controller');
 const { addEmployeeRequest, getEmployeeRequest, editEmployeeRequest } = require('./requests');
     
-app.router.addRoute('/dataService',employeeController.add,'POST').middleware([addEmployeeRequest]);
-app.router.addRoute('/dataService/@id',employeeController.get,'GET').middleware([getEmployeeRequest]);
-app.router.addRoute('/dataService',employeeController.edit,'PUT').middleware([editEmployeeRequest]);
+app.router.addRoute('/dataService/',employeeController.add,'POST').middleware([addEmployeeRequest]);
+app.router.addRoute('/dataService/@id/',employeeController.get,'GET').middleware([getEmployeeRequest]);
+app.router.addRoute('/dataService/',employeeController.edit,'PUT').middleware([editEmployeeRequest]);
 
 
   
