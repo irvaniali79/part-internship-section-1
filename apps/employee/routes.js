7cb1a464865f12420874852c9ed84618f045b083
ali.iravani <irvaniali79@gmail.com>
2022-11-28 08:39:47 +0330

    refactor(middlewares): move middlewares to core of brogrammers

--- apps/employee/routes.js
+++ apps/employee/routes.js
@@ -1,11 +1,10 @@
 const {app} = require('../..');
 const employeeController = require('./controllers/employee.controller');
-const middlewares = require('../../middlewares');
 const { addEmployeeRequest, getEmployeeRequest, editEmployeeRequest } = require('./requests');
     
-app.router.addRoute('/dataService',employeeController.add,'POST').middleware([middlewares.fetchFromBody,addEmployeeRequest]);
+app.router.addRoute('/dataService',employeeController.add,'POST').middleware([addEmployeeRequest]);
 app.router.addRoute('/dataService/@id',employeeController.get,'GET').middleware([getEmployeeRequest]);
-app.router.addRoute('/dataService',employeeController.edit,'PUT').middleware([middlewares.fetchFromBody,editEmployeeRequest]);
+app.router.addRoute('/dataService',employeeController.edit,'PUT').middleware([editEmployeeRequest]);
 
 
   
--- middlewares/index.js
+++
@@ -1,6 +0,0 @@
-const fetchQueryStringFromURL = require('./fetchQueryStringFromURL');
-const fetchFromParams = require('./fetchFromParams');
-const fetchFromBody = require('./fetchFromBody');
-const fetchFileFromFormData = require('./fetchFileFromFormData');
-
-module.exports = {fetchQueryStringFromURL,fetchFileFromFormData,fetchFromParams,fetchFromBody};
